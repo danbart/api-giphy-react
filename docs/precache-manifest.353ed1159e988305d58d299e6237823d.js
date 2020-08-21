a4ebfde3d6ed20f3cb929d881a1bdc6bd29f8920
Danilo Solórzano <danilo_solorzano06@hotmail.com>
2020-08-20 22:37:48 -0600

    docs add repository

---
+++ docs/precache-manifest.353ed1159e988305d58d299e6237823d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "58685ae2278654499e0a894dd8b0c43c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b3a1e931027db1d9dc52",
+    "url": "/static/css/2.e2dd9b0a.chunk.css"
+  },
+  {
+    "revision": "8e72f52f6c7b03638013",
+    "url": "/static/css/main.f68fb54b.chunk.css"
+  },
+  {
+    "revision": "b3a1e931027db1d9dc52",
+    "url": "/static/js/2.45f8aa0d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.45f8aa0d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8e72f52f6c7b03638013",
+    "url": "/static/js/main.f6c8fd87.chunk.js"
+  },
+  {
+    "revision": "2beaf3c9299542d4240e",
+    "url": "/static/js/runtime-main.c8ba4116.js"
+  }
+]);
